fafd908baf1096ff542a00baef55e35d430e4cbc
stwykd <5012027+stwykd@users.noreply.github.com>
2020-08-03 21:56:31 +0100

    Compare policies in slime volley

--- slimevolley/agent.py
+++ slimevolley/agent.py
@@ -1,10 +1,9 @@
 import gym
 import slimevolleygym
 
+env = gym.make("SlimeVolley-v0")
 
-def slimevolley(policy):
-    env = gym.make("SlimeVolley-v0")
-
+def run(policy):
     obs = env.reset()
     done = False
     total_reward = 0
@@ -16,3 +15,23 @@ def slimevolley(policy):
       env.render()
 
     print("score:", total_reward)
+
+def compare(policy1, policy2):
+    obs1 = env.reset()
+    obs2 = obs1  # both sides always see the same initial observation.
+
+    done = False
+    total_reward = 0
+
+    while not done:
+        action1 = policy1(obs1)
+        action2 = policy2(obs2)
+
+        obs1, reward, done, info = env.step(action1, action2)  # extra argument
+        obs2 = info['otherObs']
+
+        total_reward += reward
+        env.render()
+
+    print("policy1's score:", total_reward)
+    print("policy2's score:", -total_reward)
